255788df9a5086bfc5edc6389e27edeacc5d49dd
xenoryt <ty.cx@hotmail.com>
2015-01-02 17:21:02 -0500

    Added input handling and turned off echo

--- main.c
+++ main.c
@@ -5,13 +5,26 @@ int main()
 {
     initscr();
     init_colors();
+	noecho();
     
     WINDOW * board = new_board();
     mvwprintw(board, 1, 1, "00112233445566778899");
     
     refresh();
     wrefresh(board);
-    getch();
+	int ch;
+    while ((ch = getch()) != 'q') {
+		switch(ch) {
+			case KEY_LEFT:
+				break;
+			case KEY_RIGHT:
+				break;
+			case KEY_UP:
+				break;
+			case KEY_DOWN:
+				break;
+		}
+	}
     endwin();
 
     return 0;
