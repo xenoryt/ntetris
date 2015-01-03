e632b87c638c3ecfab23adac4262cc4daa114e55
xenoryt <ty.cx@hotmail.com>
2015-01-02 20:17:08 -0500

    Hardcoded in all the possible tetronimoes and their rotations

--- board.c
+++ board.c
@@ -4,6 +4,8 @@
 const int ROWS = 20;
 const int COLUMNS = 10 * 2; //each block is 2 wide
 
+struct tetro tetrotypes[7];
+
 void init_colors()
 {
     start_color();
@@ -18,32 +20,126 @@ void init_colors()
     init_pair(7, COLOR_WHITE, COLOR_WHITE);
 }
 
+void init_tetros() {
+	//y, x
+	int orientations[][4][4][2] = {
+		/* [][]
+		   [][]
+		*/
+		{
+			{{0, 0}, {0, -1}, {-1, 0}, {-1, -1}},
+			{{0, 0}, {0, -1}, {-1, 0}, {-1, -1}},
+			{{0, 0}, {0, -1}, {-1, 0}, {-1, -1}},
+			{{0, 0}, {0, -1}, {-1, 0}, {-1, -1}}
+		},
+		/* [][][][]    []
+					   []
+					   []
+					   []
+		*/
+		{
+			{{0, -2}, {0,-1}, {0, 0}, {0, 1}},
+			{{-1, 0}, {0, 0}, {1, 0}, {2, 0}},
+			{{0, -2}, {0,-1}, {0, 0}, {0, 1}},
+			{{-1, 0}, {0, 0}, {1, 0}, {2, 0}}
+
+		},
+
+		/*    [][]    []
+			[][]      [][]
+						[]
+		*/
+		{
+			{{0, 0}, {-1, -1}, {-1, 0}, {0, 1}},
+			{{0, 0}, {1, 0}, {0, 1}, {-1, 1}},
+			{{0, 0}, {-1, -1}, {-1, 0}, {0, 1}},
+			{{0, 0}, {1, 0}, {0, 1}, {-1, 1}}
+		},
+		
+		/* [][]         []
+			 [][]     [][]
+					  []
+		*/
+		{
+			{{0, -1}, {0, 0}, {1,0}, {1,1}}, 
+			{{-1, 1}, {0, 1}, {0,0}, {1,0}},
+			{{0, -1}, {0, 0}, {1,0}, {1,1}}, 
+			{{-1, 1}, {0, 1}, {0,0}, {1,0}}
+		},
+		
+		/*  [][][]    []            []     [][]
+			[]        []        [][][]       []
+					  [][]                   []
+		*/      
+		{
+			{{0, 0}, {0, 1}, {0, -1}, {-1, -1}},
+			{{0, 0}, {1, 0}, {-1, 0}, {-1, 1}},
+			{{0, 0}, {0, -1}, {0, 1}, {1, 1}},
+			{{0, 0}, {-1, 0}, {1, 0}, {1, -1}}
+		},
+		
+		
+		/* [][][]    [][]    []         []
+			   []    []      [][][]     []
+					 []               [][]
+		*/
+		{
+			{{0, -1}, {0,0}, {0, 1}, {1, 1}},
+			{{1, 0}, {0,0}, {-1,0},{-1, 1}},
+			{{-1, -1}, {0, -1}, {0, 0}, {0,1}},
+			{{-1, 1}, {0, 0}, {1, 0}, {-1, -1}}
+		},
+		
+		/*  [][][]    []       []      []
+			  []      [][]   [][][]  [][]
+					  []               []
+		*/
+		{
+			{{0, 0}, {0, -1}, {0, 1}, {-1, 0}},
+			{{0, 0}, {1, 0}, {-1, 0}, {0, 1}},
+			{{0, 0}, {0, -1}, {0, 1}, {1, 0}},
+			{{0, 0}, {1, 0}, {-1, 0}, {0, -1}}
+		}
+	};
+
+	int i,j,k;
+	for(i=0;i<7;i++) {
+		tetrotypes[i].rot = 0;
+		for(j=0;j<4;j++){
+			for(k=0;k<4;k++) {
+				tetrotypes[i].blocks[j][k].y = orientations[i][j][k][0];
+				tetrotypes[i].blocks[j][k].x = orientations[i][j][k][1];
+			}
+		}
+	}
+}
+
 WINDOW * new_board()
 {
-    WINDOW * board = newwin(ROWS + 2, COLUMNS + 2, 2, 2); //+2 for borders
-    box(board, 0, 0);
+	WINDOW * board = newwin(ROWS + 2, COLUMNS + 2, 2, 2); //+2 for borders
+	box(board, 0, 0);
 
-    return board;
+	return board;
 }
 
 void update_board(WINDOW * board)
 {
-    static int i = 1;
-    struct block temp;
-    temp.y = i;
-    temp.x = i;
-    temp.color = 2;
+	static int i = 1;
+	struct block temp;
+	temp.y = i;
+	temp.x = i;
+	temp.color = 2;
 
-    i++;
+	i++;
 
-    print_block(board, &temp);
+	print_block(board, &temp);
 }
 
 static void print_block(WINDOW * board, const struct block const * block)
 {
-    wattron(board, COLOR_PAIR(block->color));
-    mvwprintw(board, block->y, 1 + block->x * 2, "te");
-    wattroff(board, COLOR_PAIR(block->color));
+	wattron(board, COLOR_PAIR(block->color));
+	mvwprintw(board, block->y, 1 + block->x * 2, "te");
+	wattroff(board, COLOR_PAIR(block->color));
 }
 
 
